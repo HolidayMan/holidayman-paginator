785de3fa2f1319ffcd7ec85d29e88193f4f64bd9
HolidayMan <lesha.pitenko791@gmail.com>
2020-03-22 16:59:29 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='holidayman-paginator',
+    name='let45fc-paginator',
     version="0.1",
     author="let45fc",
     author_email="let45fc@gmail.com",
